eaa6250d2d22959cc7f98937ba702f424fe2a6b6
Mathias Krill <mathiaskrill@Mathiass-MBP.fritz.box>
2017-12-26 14:18:43 +0100

    chg: added gallery pick

--- app/src/main/java/com/company/yolo/photosniper/MainActivity.java
+++ app/src/main/java/com/company/yolo/photosniper/MainActivity.java
@@ -1,10 +1,16 @@
 package com.company.yolo.photosniper;
 
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Base64;
 import android.util.Log;
@@ -19,7 +25,6 @@ public class MainActivity extends AppCompatActivity {
     Button buttonConnectToServer;
     TextView textViewServerResult;
     TextView textViewServerStatus;
-
     EditText editTextServerAdress;
 
 
@@ -53,30 +58,7 @@ public class MainActivity extends AppCompatActivity {
         buttonConnectToServer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                ServerConnectionWebsocket serverConnectionWebsocket = new ServerConnectionWebsocket();
-                serverConnectionWebsocket.setWebSocketAdresse(editTextServerAdress.getText().toString());
-                serverConnectionWebsocket.send("yoloooo".getBytes(), new ServerConnectionWebsocket.ServerResultCallback() {
-                    @Override
-                    public void serverConnected(final String status) {
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                textViewServerStatus.setText(status);
-                            }
-                        });
-                    }
-
-                    @Override
-                    public void resultCallback(final String result) {
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                textViewServerResult.setText(result);
-                            }
-                        });
-                    }
-                });
+                willPickGalleryItem(MainActivity.this);
             }
         });
 
@@ -91,36 +73,63 @@ public class MainActivity extends AppCompatActivity {
 
             if (resultCode == RESULT_OK) {
 
-                ServerConnectionWebsocket serverConnectionWebsocket = new ServerConnectionWebsocket();
-                serverConnectionWebsocket.setWebSocketAdresse(editTextServerAdress.getText().toString());
-                serverConnectionWebsocket.send(ImageHandler.getInstance().getImage(), new ServerConnectionWebsocket.ServerResultCallback() {
-                    @Override
-                    public void serverConnected(final String status) {
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                textViewServerStatus.setText(status);
-                            }
-                        });
-                    }
+                byte[] image = ImageHandler.getInstance().getImage();
+                if (image == null) {
+                    Log.e("MainActivity", "image form Imagehandler was null [-] MainActivity [-] onActivityResult ");
 
-                    @Override
-                    public void resultCallback(final String result) {
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                textViewServerResult.setText(result);
-                            }
-                        });
-                    }
-                });
+                    return;
+                }
 
+                sendDataToServer(image);
 
             } else {
                 Log.e("MainActivity", "result was not okay  [-] MainActivity [-] onActivityResult ");
             }
 
+        } else if (requestCode == GALLERY_REQUEST) {
+            if (data != null) {
+
+                // Get the data from the uri
+                Uri selectedUri = data.getData();
+
+                // Read the file from the uri with inputstream - there is no alternative to that.
+                byte[] byteArrayUri = (ToolFiles.readFileStreamFromUriToByteArray(this, selectedUri));
+
+                if (byteArrayUri == null) {
+                    Log.e("MainActivity", "result of gallery pick had no data[-] MainActivity [-] onActivityResult [-] GALLERY_REQUEST ");
+                    return;
+                }
+
+                sendDataToServer(byteArrayUri);
+
+            }
         }
+    }
+
+    public void sendDataToServer(byte[] data){
+        ServerConnectionWebsocket serverConnectionWebsocket = new ServerConnectionWebsocket();
+        serverConnectionWebsocket.setWebSocketAdresse(editTextServerAdress.getText().toString());
+        serverConnectionWebsocket.send(data, new ServerConnectionWebsocket.ServerResultCallback() {
+            @Override
+            public void serverConnected(final String status) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        textViewServerStatus.setText(status);
+                    }
+                });
+            }
+
+            @Override
+            public void resultCallback(final String result) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        textViewServerResult.setText(result);
+                    }
+                });
+            }
+        });
     }
 
     public static String encodeToBase64String(byte[] file) {
@@ -129,4 +138,69 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    public static int READ_EXTERNAL_STORAGE_PERM = 2114;
+    public static int GALLERY_REQUEST = 1153;
+
+
+    private void willPickGalleryItem(Activity activity) {
+
+        String[] requiredPermission = new String[]{Manifest.permission.READ_EXTERNAL_STORAGE};
+
+        if (permissionsGranted(requiredPermission, activity)) {
+            Log.d("Permissions", "permissions already granted for pickGalleryItem [-] ToolPermissions [-] willPickGalleryItem");
+            pickGalleryItem(activity);
+        } else {
+
+            Log.d("Permissions", "permissions not granted for pickGalleryItem [-] ToolPermissions [-] willPickGalleryItem");
+            requestAndroidPermissoins(activity, requiredPermission, READ_EXTERNAL_STORAGE_PERM);
+        }
+    }
+
+    private void pickGalleryItem(Activity activity) {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+        activity.startActivityForResult(Intent.createChooser(intent, "Pick a image"),
+                GALLERY_REQUEST);
+    }
+
+    public boolean permissionGranted(String permission, Activity activity) {
+        if (ContextCompat.checkSelfPermission(activity.getApplicationContext(), permission)
+                != PackageManager.PERMISSION_GRANTED) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean permissionsGranted(String[] permission,Activity activity) {
+        StringBuilder permissionString = new StringBuilder();
+        for (String perm : permission) {
+            permissionString.append(" ").append(perm);
+        }
+
+        for (String perm : permission) {
+            if (!permissionGranted(perm,activity)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    public void requestAndroidPermissoins(Activity activity, String[] permission, int permissionRequest) {
+        ActivityCompat.requestPermissions(activity, permission, permissionRequest);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+
+        if (requestCode == READ_EXTERNAL_STORAGE_PERM) {
+
+            // If request is cancelled, the result arrays are empty.
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                willPickGalleryItem(this);
+            }
+        }
+    }
 }
---
+++ app/src/main/java/com/company/yolo/photosniper/ToolPermissions.java
@@ -0,0 +1,20 @@
+package com.company.yolo.photosniper;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+
+/**
+ * Created by mathiaskrill on 12/26/17.
+ */
+
+public class ToolPermissions {
+
+}
+
+
